dcb40677fa7ad1ceac9498d07e2aa23d292eb9f2
Gabriel Mouallem <gabriel.unifei2017@gmail.com>
2023-02-26 21:04:32 -0300

    feat: stop drawing when mouse leave

--- src/components/DrawableCanvas/DrawableCanvas.tsx
+++ src/components/DrawableCanvas/DrawableCanvas.tsx
@@ -10,6 +10,7 @@ export const DrawableCanvas = () => {
       ref={canvasRef}
       onMouseDown={startDrawing}
       onMouseUp={finishDrawing}
+      onMouseLeave={finishDrawing}
       onMouseMove={draw}
     />
   );
